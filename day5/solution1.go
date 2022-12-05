dc82bd11a0ad85f3a3667872d4bb8025ea885afd
phootip <phootip@gmail.com>
2022-12-05 18:07:46 +0700

    day5 sol1

--- day5/solution1.go
+++ day5/solution1.go
@@ -9,13 +9,37 @@ import (
 
 func Sol1() string {
 	fmt.Println("Starting Day5 Solution1...")
-	// raw := utils.ReadFile("./day5/input.txt")
-	raw := utils.ReadFile("./day5/example1.txt")
+	raw := utils.ReadFile("./day5/input.txt")
+	// raw := utils.ReadFile("./day5/example1.txt")
 	ans := ""
-	crates, inst := parseRaw(raw)
+	crates, insts := parseRaw(raw)
 	fmt.Println(crates)
-	fmt.Println(inst)
+	fmt.Println(insts)
+	for _, inst := range insts {
+		crates = moveN(crates, inst)
+		fmt.Println(inst)
+		fmt.Println(crates)
+	}
+	for _, s := range crates {
+		if len(s) > 0 {
+			ans += string(s[len(s)-1])
+		}
+	}
 	return ans
+}
+
+func moveN(crates [10]string, inst []int) [10]string {
+	for i := 0; i < inst[0]; i++ {
+		crates = move(crates, inst[1], inst[2])
+	}
+	return crates
+}
+
+func move(crates [10]string, s int, d int) [10]string {
+	c := crates[s][len(crates[s])-1]
+	crates[s] = crates[s][:len(crates[s])-1]
+	crates[d] = crates[d] + string(c)
+	return crates
 }
 
 func parseRaw(raw []string) ([10]string, [][]int) {
@@ -39,7 +63,7 @@ func parseRaw(raw []string) ([10]string, [][]int) {
 				}
 			}
 		} else {
-		// parse inst
+			// parse inst
 			fmt.Println("Parsing Inst...")
 			if line == "" {
 				break
