c0b76767ca052b51cf1f0d7d2806c34d3863fee5
phootip <phootip.tulayavorases@binance.com>
2022-12-13 21:07:58 +0700

    day13 part1

--- day13/solution1.go
+++ day13/solution1.go
@@ -8,33 +8,86 @@ import (
 
 func Sol1() int {
 	fmt.Println("Starting Day13 Solution1...")
-	// raw := utils.ReadFile("./day13/input.txt")
-	raw := utils.ReadFile("./day13/example.txt")
+	raw := utils.ReadFile("./day13/input.txt")
+	// raw := utils.ReadFile("./day13/example.txt")
 	// raw := utils.ReadFile("./day13/example1.txt")
-	rawToParis(raw)
+	pairs := rawToParis(raw)
+	ans := 0
+	for i, aPair := range pairs {
+		if compare(aPair.first, aPair.second) == 1 {
+			ans += i + 1
+			// fmt.Println("ans: ", ans)
+		}
+		// fmt.Println()
+	}
+	return ans
+}
+
+func compare(a, b any) int {
+	a = ifIntToSlice(a)
+	b = ifIntToSlice(b)
+	// fmt.Println(a,"---", b)
+	for i := range a.([]any) {
+		// fmt.Println("i: ", i)
+		// fmt.Println(a.([]any)[i])
+		// fmt.Println(b.([]any)[i])
+		if i > len(b.([]any))-1 {
+			return -1
+		}
+		cInt, cIsInt := a.([]any)[i].(int)
+		dInt, dIsInt := b.([]any)[i].(int)
+		// fmt.Println(cInt, dInt)
+		if !(cIsInt && dIsInt) {
+			subResult := compare(a.([]any)[i], b.([]any)[i])
+			// fmt.Println("subResult: ", subResult)
+			if subResult != 0 {
+				return subResult
+			}
+		} else {
+			if cInt < dInt {
+				return 1
+			}
+			if cInt > dInt {
+				return -1
+			}
+		}
+	}
+	if len(a.([]any)) < len(b.([]any)) {
+		return 1
+	}
 	return 0
 }
 
-func rawToParis(raw []string) {
+func ifIntToSlice(a any) any {
+	var c any
+	switch b := a.(type) {
+	case int:
+		c = []any{b}
+	default:
+		c = b
+	}
+	return c
+}
+
+func rawToParis(raw []string) []pair {
 	state := "first"
-	data := pair[int]{}
-	_ = data
+	result := []pair{}
+	aPair := pair{}
 	for _, line := range raw {
 		if line == "" {
 			continue
 		}
 		switch state {
 		case "first":
-			a, _ := lineToSignal(line[1:])
-			fmt.Println(a)
+			aPair.first, _ = lineToSignal(line[1:])
 			state = "second"
 		case "second":
-			a, _ := lineToSignal(line[1:])
-			fmt.Println(a)
-			fmt.Println()
+			aPair.second, _ = lineToSignal(line[1:])
+			result = append(result, aPair)
 			state = "first"
 		}
 	}
+	return result
 }
 
 func lineToSignal(line string) (result []any, skipTo int) {
@@ -57,7 +110,7 @@ func lineToSignal(line string) (result []any, skipTo int) {
 			if temp != "" {
 				result = append(result, utils.StringToInt(temp))
 			}
-			return result, i+1
+			return result, i + 1
 		case ",":
 			if temp == "" {
 				continue
@@ -72,8 +125,7 @@ func lineToSignal(line string) (result []any, skipTo int) {
 	return result, -1
 }
 
-
-type pair[T int | []T] struct {
-	first  []T
-	second []T
+type pair struct {
+	first  []any
+	second []any
 }
