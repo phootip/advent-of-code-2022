3f171c89ec7e360a2a4c4d65c5dd6fc967fa8042
phootip <phootip.tulayavorases@binance.com>
2022-12-13 21:56:29 +0700

    You already build the compare brah

--- day13/solution2.go
+++ day13/solution2.go
@@ -13,21 +13,24 @@ func Sol2() int {
 	raw := utils.ReadFile("./day13/input.txt")
 	// raw := utils.ReadFile("./day13/example.txt")
 	// raw := utils.ReadFile("./day13/example1.txt")
-	raw = append(raw, "[[2]]", "[[6]]")
+	// raw = append(raw, "[[2]]", "[[6]]")
 	ans := 1
 	packets := rawToPackets(raw)
+	divider := rawToPackets([]string{"[[2]]", "[[6]]"})
+	packets = append(packets, divider[0], divider[1])
 	sort.Slice(packets, func(i, j int) bool {
 		return compare(packets[i], packets[j]) == 1
 	})
 	for i, packet := range packets {
-		if checkDivider(packet) {
-			ans *= i+1
+		// if checkDivider(packet) {
+		if compare(divider[0], packet) == 0 ||  compare(divider[1], packet) == 0 {
+			ans *= i + 1
 		}
 	}
 	return ans
 }
 
-func checkDivider(packet any) bool{
+func checkDivider(packet any) bool {
 	slice := packet.([]any)
 	if len(slice) == 1 {
 		if reflect.TypeOf(slice[0]).Kind() == reflect.Slice && len(slice[0].([]any)) == 1 {
