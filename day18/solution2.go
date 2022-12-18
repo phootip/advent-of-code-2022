50324518c8f51bd03d0afc18bd28670e269445eb
phootip <phootip@gmail.com>
2022-12-18 17:04:25 +0700

    day18 bfs not working

--- day18/solution2.go
+++ day18/solution2.go
@@ -9,8 +9,8 @@ import (
 // 4266 too high
 func Sol2() (ans int) {
 	fmt.Println("Starting Day18 Solution2...")
-	raw := utils.ReadFile("./day18/input.txt")
-	// raw := utils.ReadFile("./day18/example.txt")
+	// raw := utils.ReadFile("./day18/input.txt")
+	raw := utils.ReadFile("./day18/example.txt")
 	raw = raw[:len(raw)-1]
 	coords := rawToCoords(raw)
 	ans = countSides2(coords)
@@ -18,35 +18,97 @@ func Sol2() (ans int) {
 }
 
 func countSides2(coords [][3]int) (ans int) {
-	// mem := [][][3]int{}
-	for i, coor := range coords {
-		ans += 6
-		for _, coor2 := range coords[:i] {
-			if adjacent(coor, coor2) {
-				ans -= 2
-			}
-		}
-		trappedSides := checkTrapped(coor, coords[:i], coords)
-		ans -= trappedSides*6
+	minZ, maxZ := getMinMaxZ(coords)
+	grouped := groupCoordsByZ(coords, minZ, maxZ)
+	// fmt.Println(coords)
+	// fmt.Println(grouped)
+	start := grouped[minZ][0]
+	ans = bfs(start, coords)
+	return ans
+}
 
+func bfs(root [3]int, coords [][3]int) int{
+	var node [3]int
+	queue := [][3]int{root}
+	visited := map[[3]int]bool{}
+	visited[root] = true
+	ans := 6
+
+	for len(queue) > 0 {
+		node, queue = queue[0], queue[1:]
+		adjNodes := getAdjCoords(node)
+		fmt.Println("node: ", node)
+		fmt.Println(adjNodes)
+
+		for _, adjNode := range adjNodes{
+			if !coordExist(adjNode, coords) || visited[adjNode] {
+				continue
+			}
+			fmt.Println("visiting node: ", adjNode)
+			queue = append(queue, adjNode)
+			visited[adjNode] = true
+			ans += 6
+		}
 	}
 	return ans
+
+	// visited := map[int]bool{}
+
+}
+
+func getAdjCoords(node [3]int) (adjCoords [][3]int) {
+	temp := node
+	temp[2]--
+	adjCoords = append(adjCoords, temp)
+	for _, axis := range [3]int{0, 1} {
+		for _, dir := range [2]int{-1, 1} {
+			temp = node
+			temp[axis] += dir
+			adjCoords = append(adjCoords, temp)
+		}
+	}
+	temp = node
+	temp[2]++
+	adjCoords = append(adjCoords, temp)
+	return adjCoords
+}
+
+func groupCoordsByZ(coords [][3]int, minZ int, maxZ int) map[int][][3]int {
+	result := map[int][][3]int{}
+	for z := minZ; z <= maxZ; z++ {
+		result[z] = filterCoordWithZ(coords, z)
+	}
+	return result
+}
+
+func getMinMaxZ(coords [][3]int) (int, int) {
+	minZ := 1000
+	maxZ := 0
+	for _, coord := range coords {
+		minZ = utils.Min(minZ, coord[2])
+		maxZ = utils.Max(maxZ, coord[2])
+	}
+	return minZ, maxZ
+}
+
+func filterCoordWithZ(coords [][3]int, z int) (result [][3]int) {
+	for _, coord := range coords {
+		if coord[2] == z {
+			result = append(result, coord)
+		}
+	}
+	return result
 }
 
 func checkTrapped(coord1 [3]int, coords [][3]int, universeCoords [][3]int) int {
 	// fmt.Println("checkTrapped for: ", coord1)
 	// fmt.Println("List to check: ", coords)
-
-	// check for one side trapped
-	// checking Z
-	// check no adjacent
 	result := 0
 	for _, axis := range [3]int{0, 1, 2} {
 		for _, dir := range [2]int{-2, 2} {
 			if checkTrappedAxis(coord1, coords, universeCoords, axis, dir) {
 				result += 1
 			}
-			// fmt.Println("result: ", result)
 		}
 	}
 	return result
